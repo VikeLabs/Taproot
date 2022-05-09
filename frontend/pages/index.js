5dbd1f5470815a91dca244ce991574001d31ba08
Ian DeJong <ianrdejong@gmail.com>
2022-05-08 17:57:24 -0700

    Fix console errors

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -33,16 +33,16 @@ export default function Home() {
 
         {/* <Post pid={pid}/> */}
 
-        <div class="list-group">
-          <a href="carrots" class="list-group-item list-group-item-action">The current link item</a>
-          <a href="foo" class="list-group-item list-group-item-action">A second link item</a>
-          <a href="bar" class="list-group-item list-group-item-action">A third link item</a>
-          <a href="gabba" class="list-group-item list-group-item-action">A fourth link item</a>
+        <div className="list-group">
+          <a href="carrots" className="list-group-item list-group-item-action">The current link item</a>
+          <a href="foo" className="list-group-item list-group-item-action">A second link item</a>
+          <a href="bar" className="list-group-item list-group-item-action">A third link item</a>
+          <a href="gabba" className="list-group-item list-group-item-action">A fourth link item</a>
         </div>
 
         {/* Include Boostrap via CDN */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
       </div>
     </ApolloProvider>
   )
